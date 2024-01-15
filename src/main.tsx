49ca9b350372bacfbe139a64c734748effaeb0d8
Ian Stewart <ianstewart737@gmail.com>
2024-01-14 22:31:50 -0500

    Create word node game

--- src/main.tsx
+++ src/main.tsx
@@ -6,17 +6,12 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import Home from './pages/Home/Home';
-import SurvivorReel from './pages/SurvivorReel/SurvivorReel';
 import WordPathGame from './pages/WordPathGame/WordPathGame';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
-  },
-  {
-    path: '/survivor-reel',
-    element: <SurvivorReel />,
   },
   {
     path: '/word-node-game',
